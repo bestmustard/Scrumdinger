c25e5d86cbdc124ada8cf01ad9e32ca62c618dfd
Victor Ma <victor.ma11@icloud.com>
2023-05-03 12:25:00 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Victor Ma on 2023-05-03.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
